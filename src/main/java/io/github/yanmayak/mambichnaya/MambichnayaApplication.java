8d79753a4943ec9c25570ac580fc82926d5a2033
Marina Dvoretskova <smallestproblem@gmail.com>
2025-06-07 11:20:05 +0300

    init

---
+++ src/main/java/io/github/yanmayak/mambichnaya/MambichnayaApplication.java
@@ -0,0 +1,13 @@
+package io.github.yanmayak.mambichnaya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MambichnayaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MambichnayaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/yanmayak/mambichnaya/MambichnayaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.yanmayak.mambichnaya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MambichnayaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
