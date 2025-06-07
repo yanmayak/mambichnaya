53e9df154ecacd9cea60897e58fee23917528fd5
Marina Dvoretskova <smallestproblem@gmail.com>
2025-06-07 12:20:26 +0300

    added dependencies, disabled tests

--- src/test/java/io/github/yanmayak/mambichnaya/MambichnayaApplicationTests.java
+++ src/test/java/io/github/yanmayak/mambichnaya/MambichnayaApplicationTests.java
@@ -1,9 +1,11 @@
 package io.github.yanmayak.mambichnaya;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled
 class MambichnayaApplicationTests {
 
     @Test
