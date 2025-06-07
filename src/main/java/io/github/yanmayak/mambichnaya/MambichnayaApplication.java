0c9b26660bfa73c0737ca0d720398b68268e56a5
Marina Dvoretskova <smallestproblem@gmail.com>
2025-06-07 15:51:22 +0300

    disabled jpa

--- src/main/java/io/github/yanmayak/mambichnaya/MambichnayaApplication.java
+++ src/main/java/io/github/yanmayak/mambichnaya/MambichnayaApplication.java
@@ -2,10 +2,12 @@ package io.github.yanmayak.mambichnaya;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
 @EnableFeignClients
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class MambichnayaApplication {
 
     public static void main(String[] args) {
